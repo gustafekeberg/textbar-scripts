15d28f4fb90e53c022c1e78111499fa35925103a
Gustaf Ekeberg <gvb.ekeberg@gmail.com>
2015-12-22 23:04:33 +0100

    Bookmark config order reversed to `url;name`. If `name` is empty `url` will be used instead.

--- bookmarks.zsh
+++ bookmarks.zsh
@@ -25,7 +25,7 @@ case $TEXTBAR_TEXT in
 		;;
 	* )
 		if [[ -n $TEXTBAR_INDEX && $TEXTBAR_TEXT != $divider  ]]; then
-			url=`echo ${bookmarks[$TEXTBAR_INDEX]} | cut -d ${delimiter} -f 2`
+			url=`echo ${bookmarks[$TEXTBAR_INDEX]} | cut -d ${delimiter} -f 1`
 			open "${url}"
 			exit
 		fi
@@ -36,7 +36,11 @@ esac
 echo $icon$menubartext
 for item in $bookmarks
 do
-	name=`echo ${item} | cut -d ${delimiter} -f 1`
+	name=`echo ${item} | cut -d ${delimiter} -f 2`
+	url=`echo ${item} | cut -d ${delimiter} -f 1`
+	if [[ -z $name ]]; then
+		name=$url
+	fi
 	echo $name
 done
 
