e34dea9c0de3c43c4757127734e807aaf4c8d629
Gustaf Ekeberg <gustaf@gustafekeberg.se>
2016-08-06 22:32:54 +0200

    Added monkeypatch to work with Exchange/Microsoft IMAP

--- mailbox-counter.rb
+++ mailbox-counter.rb
@@ -3,6 +3,30 @@
 
 require 'rubygems'
 require 'net/imap'
+# Monkeypatching the Ruby IMAP class to build a client for Exchange email servers not compliant with RFC standards
+# http://claudiofloreani.blogspot.se/2012/01/monkeypatching-ruby-imap-class-to-build.html
+module Net
+	class IMAP
+		class ResponseParser
+			def response
+				token = lookahead
+				case token.symbol
+				when T_PLUS
+					result = continue_req
+				when T_STAR
+					result = response_untagged
+				else
+					result = response_tagged
+				end
+				match(T_SPACE) if lookahead.symbol == T_SPACE
+				match(T_CRLF)
+				match(T_EOF)
+				return result
+			end #def response
+		end #class ResponseParser
+	end #class IMAP
+end #module Net
+
 require 'yaml'
 require 'fileutils'
 
